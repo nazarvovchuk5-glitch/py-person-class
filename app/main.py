a269746079455c81b522c2d76fa5cc3417ef0b15
Nazar Vovchuk <nazarvovchuc3002@gmail.com>
2026-02-24 13:47:51 +0200

    Solution1

--- app/main.py
+++ app/main.py
@@ -1,27 +1,23 @@
 class Person:
     people = {}
 
-    def __init__(self, name: str, age: str) -> None:
+    def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
         Person.people[self.name] = self
 
 
-def create_person_list(data: list) -> list:
-    Person.people.clear()  # очистити перед створенням
+def create_person_list(people: list) -> list:
+    Person.people.clear()
 
-    # First pass — create all persons
-    for human in data:
-        Person(human["name"], human["age"])
+    [Person(human.get("name"), human.get("age")) for human in people]
 
-    # Second pass — assign relationships
-    for human in data:
-        person = Person.people[human["name"]]
+    for person in people:
+        human = Person.people.get(person.get("name"))
+        if person.get("wife", None):
+            human.wife = Person.people.get(person["wife"])
 
-        if human.get("wife"):
-            person.wife = Person.people.get(human["wife"])
-
-        if human.get("husband"):
-            person.husband = Person.people.get(human["husband"])
+        if person.get("husband", None):
+            human.husband = Person.people.get(person["husband"])
 
     return list(Person.people.values())
